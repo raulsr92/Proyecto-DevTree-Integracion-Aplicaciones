eb1d9fcc1c807509b51175a938ee7d98ba1d07ec
Raul Sanchez R. <raul.sr92@gmail.com>
2025-06-07 16:11:07 -0500

    Levantar un servidor con Express y crear primera ruta get

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("Hola mundo desde DevTree")
+
+import express from "express"
+
+// Crear una instancia del servidor Express
+
+const app = express()
+
+// Routing
+
+app.get('/',(req,res)=>{
+    res.send("Hola mundo desde express")
+})
+
+
+// Crear (inicializar) servidor
+
+app.listen(4000,()=>{
+    console.log("Servidor Levantado")
+})
+
